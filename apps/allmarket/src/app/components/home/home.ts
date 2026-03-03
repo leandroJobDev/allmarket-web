cc542a2b406f6ff7e3a0c0cd4e4d7b2792b33996
leandroJobDev <lealjobs88@gmail.com>
2026-03-03 09:32:28 -0300

    inclusao do html e scss home

--- apps/allmarket/src/app/components/home/home.ts
+++ apps/allmarket/src/app/components/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
--- apps/allmarket/src/app/components/login/login.ts
+++ apps/allmarket/src/app/components/login/login.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { Home } from '../home/home';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    Home,
+    MatCardModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
