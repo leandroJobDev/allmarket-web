5769258638c28c24d5bb3e6d42057f8a06b0eee7
leandroJobDev <lealjobs88@gmail.com>
2026-03-10 18:10:19 -0300

    ajustes project.json

--- apps/allmarket/module-federation.config.ts
+++ apps/allmarket/module-federation.config.ts
@@ -1,14 +1,12 @@
 import { ModuleFederationConfig } from '@nx/module-federation';
 
-const isProd = process.env['NODE_ENV'] === 'production' || process.argv.includes('--configuration=production');
-
 const config: ModuleFederationConfig = {
   name: 'allmarket',
   remotes: [
-    ['notas_mfe', isProd ? 'https://allmarket-notas-mfe.web.app' : 'http://localhost:4201'],
-    ['analise_mfe', isProd ? 'https://allmarket-analise-mfe.web.app' : 'http://localhost:4202'],
-    ['comparador_mfe', isProd ? 'https://allmarket-comparador-mfe.web.app' : 'http://localhost:4203'],
-    ['listas_mfe', isProd ? 'https://allmarket-listas-mfe.web.app' : 'http://localhost:4204'],
+    ['notas_mfe', 'http://localhost:4201'],
+    ['analise_mfe', 'http://localhost:4202'],
+    ['comparador_mfe', 'http://localhost:4203'],
+    ['listas_mfe', 'http://localhost:4204'],
   ],
 };
 
--- apps/allmarket/webpack.prod.config.ts
+++ apps/allmarket/webpack.prod.config.ts
@@ -3,8 +3,13 @@ import { withModuleFederation } from '@nx/module-federation/angular';
 import config from './module-federation.config';
 
 const prodConfig: ModuleFederationConfig = {
-  ...config, 
-  remotes: [],
+  ...config,
+  remotes: [
+    ['notas_mfe', 'https://allmarket-notas-mfe.web.app'],
+    ['analise_mfe', 'https://allmarket-analise-mfe.web.app'],
+    ['comparador_mfe', 'https://allmarket-comparador-mfe.web.app'],
+    ['listas_mfe', 'https://allmarket-listas-mfe.web.app'],
+  ],
 };
 
 export default withModuleFederation(prodConfig, { dts: false });
