9e6e8111dbaa6119fac9b8fe3e10b2eb645d654c
leandroJobDev <lealjobs88@gmail.com>
2026-03-10 15:59:15 -0300

    troca apiUrl

--- libs/shared/src/app/environments/environment.ts
+++ libs/shared/src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   googleClientId: "570724598871-n23jsilb8ncvfv2ve6b848q327fgdav9.apps.googleusercontent.com",
-  apiUrl: 'https://allmarket-api.onrender.com',
+  apiUrl: 'https://allmarket-api-go.onrender.com',
   notasMfeUrl: 'http://localhost:4201',
   analiseMfeUrl: 'http://localhost:4202',
   comparadorMfeUrl: 'http://localhost:4203',
