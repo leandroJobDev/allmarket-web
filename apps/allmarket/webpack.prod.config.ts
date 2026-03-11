d50b34e65f571d8cd39a7011799f9052c596532d
leandroJobDev <lealjobs88@gmail.com>
2026-03-11 14:56:55 -0300

    fix rotas

--- apps/allmarket/webpack.prod.config.ts
+++ apps/allmarket/webpack.prod.config.ts
@@ -1,15 +1,7 @@
-import { ModuleFederationConfig } from '@nx/module-federation';
 import { withModuleFederation } from '@nx/module-federation/angular';
 import config from './module-federation.config';
 
-const prodConfig: ModuleFederationConfig = {
+export default withModuleFederation({
   ...config,
-  remotes: [
-    ['notas_mfe', 'https://allmarket-notas-mfe.web.app'],
-    ['analise_mfe', 'https://allmarket-analise-mfe.web.app'],
-    ['comparador_mfe', 'https://allmarket-comparador-mfe.web.app'],
-    ['listas_mfe', 'https://allmarket-listas-mfe.web.app'],
-  ],
-};
-
-export default withModuleFederation(prodConfig, { dts: false });
+  remotes: [], 
+}, { dts: false });
