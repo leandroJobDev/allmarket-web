01ac7f3e355957cc13941ae3e53014d198c32e5d
leandroJobDev <lealjobs88@gmail.com>
2026-03-11 14:14:13 -0300

    republicacao

--- libs/shared/src/app/environments/environment.ts
+++ libs/shared/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  googleClientId: "570724598871-n23jsilb8ncvfv2ve6b848q327fgdav9.apps.googleusercontent.com",
+  googleClientId: '570724598871-n23jsilb8ncvfv2ve6b848q327fgdav9.apps.googleusercontent.com',
   apiUrl: 'https://allmarket-api-go.onrender.com',
   notasMfeUrl: 'http://localhost:4201',
   analiseMfeUrl: 'http://localhost:4202',
